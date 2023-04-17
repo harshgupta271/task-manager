e9e1551c7a4cd98f78d69f82d0bac3b5e6c8b3d5
harshgupta271 <124806618+harshgupta271@users.noreply.github.com>
2023-04-17 19:30:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-const addtasks=document.queryselector('.add');
-const list=document.queryselector('tasks');
+const addtasks=document.querySelector('.add')
+const list=document.querySelector('.tasks');
 const search=document.querySelector('.search input');
-
-const generate=todo=>{
-  const html=
+const generate= task =>
+{
+  const html= `
       <li class="list-group-item d-flex justify-content-between align-items-center">
-        <span>${tasks}</span>
-       <i class="far fa-trash-alt delete"></i>;
-  list.innerHTML+=html;
+        <span>${task}</span>
+       <i class="far fa-trash-alt delete"></i>
+       </li>`
+       ;
+  list.innerHTML += html
 };
-addtasks.addEventListener('submit',e=>{
-  e,preventdefault();
-  const task=addtask.add.value.trim();
+addtasks.addEventListener('submit', e=>{
+  e.preventDefault();
+  const task =addtasks.add.value.trim();
+  if(task.length){
   generate(task);
   addtasks.reset();
-};
-    list.addEventListener('click',e=>{
-                         if(e.target.classList.contains('delete')){
-e.target.parentElement.remove();
+  }
+  
 });
+
+list.addEventListener('click',e =>{
+ if(e.target.classList.contains('delete')){
+e.target.parentElement.remove();
+}});
 const filter= (term)=>{
 Array.from(list.children)
-.filter((container)=>!container.textContent.toLowerCase().includes(term))
-.forEach((container)=>container.classList.add('filtered'));
+.filter((tasks)=>!tasks.textContent.toLowerCase().includes(term))
+.forEach((tasks)=>tasks.classList.add('filtered'));
 
 Array.from(list.children)
-.filter((container)=>container.textContent.toLowerCase().includes(term))
-.forEach((container)=>container.classList.remove('filtered'));  
+.filter((tasks) =>tasks.textContent.toLowerCase().includes(term))
+.forEach((tasks) =>tasks.classList.remove('filtered'));  
 
 };
 search.addEventListener('keyup',()=>{
